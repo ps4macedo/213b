a56823bd26ea191d7a70be6bad583f27fc5a495e
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2020-09-18 21:49:47 -0300

    Add files via upload

--- exp_kernelHEN.js
+++ exp_kernelHEN.js
@@ -33768,13 +33768,13 @@ var _ = malloc_nogc.pop();
 
 if (main_ret == 179 || main_ret == 0) {
 	localStorage.passcount = ++localStorage.passcount;window.passCounter.innerHTML=localStorage.passcount;
-	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Exploit Loaded ✔ Now Loading HEN ...</h1>";
+	window.msgs.innerHTML=<h1 style='font-size:25px;text-align:center;'><font color=\"#6089f6\">Exploit ✔ </font><font color=\"#ee596f\">HEN ... </font></h1>";
     setTimeout(function(){
     plLoader();
     },1000);
 } 
 else {
 	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
-	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Jailbreak failed! - Reboot your PS4 and try again.</h1>";
+	window.msgs.innerHTML="<h1 style='color:#ee596f;font-size:25px;text-align:center;'>Jailbreak FALHOU! - Pressione o \"CIRCULO\" e reinicie o seu PS4 pelas opções de energia do console.</h1>";
 }
 }
